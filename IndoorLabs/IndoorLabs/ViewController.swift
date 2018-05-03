560decc9a941794f37f8daddf032f1b09e7bd512
Ruben Garcia <Rubencho@iMac-de-usuario.local>
2018-05-03 18:20:41 -0500

    first commit

---
+++ IndoorLabs/IndoorLabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IndoorLabs
+//
+//  Created by Ruben Garcia on 3/05/18.
+//  Copyright © 2018 Ruben Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
